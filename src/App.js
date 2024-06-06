a8fcf4c1a3899ad57cb6ccd9faca2d520879b48c
7844Pritam <your.email@example.com>
2024-06-06 21:21:52 +0530

    savve

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import LoginPage from "./pages/Login/LoginPage";
 import SignUp from "./pages/SignUp/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Courses from "./pages/Courses/Courses/Courses";
+import Home from "./pages/Home/Home";
+import "./mastercss/InputStyle.css";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<LoginPage />} /> */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<Courses />} />
+        {/* <Route path="/" element={<LoginPage />} /> */}
+        <Route path="/" element={<SignUp />} />
+        {/* <Route path="/" element={<Courses />} /> */}
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/" element={<HomeCourses />} /> */}
         {/* Add more routes as needed */}
