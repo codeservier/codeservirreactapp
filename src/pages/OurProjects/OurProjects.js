7f05a36938f4c9b2aa153d3ec155909372b7b829
codeservir <your_email@example.com>
2024-06-22 17:34:00 +0530

    hello

--- src/pages/OurProjects/OurProjects.js
+++ src/pages/OurProjects/OurProjects.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import projectImage1 from "../../assets/techCons.png";
-import projectImage2 from "../../assets/webDev.png";
+import projectImage1 from "../../assets/projectImages/proj1_car.png";
+import projectImage2 from "../../assets/projectImages/proj2_fitnesss.png";
+import medicalimg from "../../assets/projectImages/Proj3_medical.png";
 
 const OurProjects = () => {
   const [selectedCategory, setSelectedCategory] = useState("Commercial Project");
@@ -19,14 +20,23 @@ const OurProjects = () => {
   const projects = {
     "Commercial Project": [
       {
-        title: "Office Building",
-        description: "A state-of-the-art office building.",
+        title: "Car Cleaning Mobile App",
+        description: "Professional car cleaning and detailing services.",
         image: projectImage1,
+        demoLink: "https://example.com/office-demo",
+      },
+      
+      {
+        title: "Shree Hari Medical Store App",
+        description: "A large shopping mall with various stores.",
+        image: medicalimg,
+        demoLink: "https://example.com/mall-demo",
       },
       {
-        title: "Shopping Mall",
+        title: "Shree Hari Medical Store App",
         description: "A large shopping mall with various stores.",
-        image: projectImage2,
+        image: medicalimg,
+        demoLink: "https://example.com/mall-demo",
       },
     ],
     "Teaching Project": [
@@ -34,23 +44,27 @@ const OurProjects = () => {
         title: "Online Learning Platform",
         description: "An innovative platform for online learning.",
         image: projectImage1,
+        demoLink: "https://example.com/online-learning-demo",
       },
       {
         title: "School Management System",
         description: "A comprehensive system for managing schools.",
         image: projectImage2,
+        demoLink: "https://example.com/school-management-demo",
       },
     ],
     "Gym Project": [
       {
-        title: "Fitness Center",
-        description: "A modern fitness center with advanced equipment.",
-        image: projectImage1,
+        title: "Gym Mobile App",
+        description: "A large shopping mall with various stores.",
+        image: projectImage2,
+        demoLink: "https://example.com/mall-demo",
       },
       {
         title: "Yoga Studio",
         description: "A serene yoga studio for mindfulness and meditation.",
         image: projectImage2,
+        demoLink: "https://example.com/yoga-studio-demo",
       },
     ],
     "AI Project": [
@@ -58,11 +72,13 @@ const OurProjects = () => {
         title: "AI Chatbot",
         description: "An intelligent chatbot for customer service.",
         image: projectImage1,
+        demoLink: "https://example.com/ai-chatbot-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/ml-model-demo",
       },
     ],
     "Healthcare Project": [
@@ -70,11 +86,13 @@ const OurProjects = () => {
         title: "AI Chatbot",
         description: "An intelligent chatbot for customer service.",
         image: projectImage1,
+        demoLink: "https://example.com/healthcare-chatbot-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/healthcare-ml-model-demo",
       },
     ],
     "Finance Project": [
@@ -82,11 +100,13 @@ const OurProjects = () => {
         title: "AI Chatbot",
         description: "An intelligent chatbot for customer service.",
         image: projectImage1,
+        demoLink: "https://example.com/finance-chatbot-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/finance-ml-model-demo",
       },
     ],
     "Education Project": [
@@ -94,11 +114,13 @@ const OurProjects = () => {
         title: "AI Chatbot",
         description: "An intelligent chatbot for customer service.",
         image: projectImage1,
+        demoLink: "https://example.com/education-chatbot-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/education-ml-model-demo",
       },
     ],
     "Retail Project": [
@@ -106,26 +128,31 @@ const OurProjects = () => {
         title: "AI Chatbot",
         description: "An intelligent chatbot for customer service.",
         image: projectImage1,
+        demoLink: "https://example.com/retail-chatbot-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/retail-ml-model-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/retail-ml-model-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/retail-ml-model-demo",
       },
       {
         title: "Machine Learning Model",
         description: "A cutting-edge machine learning model for data analysis.",
         image: projectImage2,
+        demoLink: "https://example.com/retail-ml-model-demo",
       },
     ],
     // Add more projects for other categories as needed
@@ -135,40 +162,57 @@ const OurProjects = () => {
 
   return (
     <div className="md:p-10 sm:mx-[2rem] md:my-16 my-7">
-      <h1 className="md:text-8xl my-7 text-4xl font-semibold md:mb-10 text-[#26baf6] font-lilita text-center">Our Projects</h1>
+      <h1 className="md:text-8xl my-7 text-4xl font-semibold md:mb-10 text-[#26baf6] font-lilita text-center">
+        Our Projects
+      </h1>
       <div className="flex flex-wrap justify-around mb-6">
         {categories.map((category) => (
           <button
             key={category}
             onClick={() => setSelectedCategory(category)}
             className={` font-concert  md:p-14 p-4 m-2 md:rounded-t rounded-full text-white md:text-4xl transition duration-300 ${
-              selectedCategory === category 
+              selectedCategory === category
                 ? "bg-[#26baf6] hover:bg-[#3ae8ff]"
-                : "bg-[#dda242] hover:bg-[#c68e33]" 
+                : "bg-[#dda242] hover:bg-[#c68e33]"
             }`}
           >
             {category}
           </button>
         ))}
       </div>
-      <div className="bg-[#ebfbff] md:p-12 p-6 rounded-lg shadow-md">
-  <h2 className=" font-lilita text-4xl font-semibold mb-4 text-center">{selectedCategory}</h2>
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-    {currentProjects.map((project, index) => (
-      <div key={index} className="bg-gray-100 p-10 rounded-lg shadow-md flex flex-col">
-        <img
-          src={project.image}
-          alt={project.title}
-          className="h-56 w-full object-cover rounded-lg mb-4"
-        />
-        <div className="flex-1">
-          <h3 className=" font-lilita text-3xl font-semibold mb-2">{project.title}</h3>
-          <p className="font-concert text-gray-700 ">{project.description}</p>
+      <div className="bg-[#fff16e] md:p-12 p-6 rounded-lg shadow-md">
+        <h2 className=" font-lilita text-4xl font-semibold mb-4 text-center">
+          {selectedCategory}
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {currentProjects.map((project, index) => (
+            <div
+              key={index}
+              className="bg-[#ffffff] p-20 rounded-lg shadow-md flex flex-col relative"
+            >
+              <img
+                src={project.image}
+                alt={project.title}
+                className="h-100 w-full object-cover rounded-lg mb-4"
+              />
+              <div className="flex-1">
+                <h3 className=" font-lilita text-3xl text-[red] font-semibold mb-2">
+                  {project.title}
+                </h3>
+                <p className="font-concert text-gray-700 ">
+                  {project.description}
+                </p>
+                <a
+                  href={project.demoLink}
+                  className="absolute bottom-4 right-4 text-[#26baf6] font-semibold cursor-pointer animate-blink"
+                >
+                  Live Demo
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
-    ))}
-  </div>
-</div>
     </div>
   );
 };
